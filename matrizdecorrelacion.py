fabcbc2408e6983243602df6c6eff8e09746849b
doctoradoiaup <doctoradoiaup@gmail.com>
2024-10-31 12:06:08 -0600

    Update matrizdecorrelacion.py

--- matrizdecorrelacion.py
+++ matrizdecorrelacion.py
@@ -9,6 +9,22 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 
+# Estilo de fondo
+page_bg_img = """
+<style>
+[data-testid="stAppViewContainer"]{
+background:
+radial-gradient(black 15%, transparent 16%) 0 0,
+radial-gradient(black 15%, transparent 16%) 8px 8px,
+radial-gradient(rgba(255,255,255,.1) 15%, transparent 20%) 0 1px,
+radial-gradient(rgba(255,255,255,.1) 15%, transparent 20%) 8px 9px;
+background-color:#282828;
+background-size:16px 16px;
+</style>
+"""
+
+st.markdown(page_bg_img, unsafe_allow_html=True)
+
 # Cargar automáticamente el archivo Excel
 file_path = 'Matriz de correlacion.xlsx'
 data = pd.read_excel(file_path)
